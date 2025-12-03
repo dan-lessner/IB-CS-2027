349d112401ce0732c5221859068d48e0be89a1b5
honza <honza.ruzicka07@gmail.com>
2025-12-03 09:39:58 +0100

    I have uploaded our previous work from the last lesson

---
+++ L-Systems/LuckaHonzaAdrian/main.py
@@ -0,0 +1,8 @@
+import math
+import random
+
+axiom = input("Axiom desired:")
+angle_given = int(input("Your desired angle:"))
+iteration = int(input("Iteration desired:"))
+
+#constants would be  inn
