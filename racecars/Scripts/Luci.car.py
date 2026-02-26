35d9899bf556b2ca6fa8455a3a3ad02581f3a757
Viki <vtampierova@gmail.com>
2026-02-26 14:56:02 +0100

    lucka

--- racecars/Scripts/Luci.car.py
+++ racecars/Scripts/Luci.car.py
@@ -1,60 +1,48 @@
 import random
-from simulation.script_api import AutoAuto, WorldState
+from simulation.script_api import AutoAuto
 
 class Auto(AutoAuto):
-    def _init_(self) -> None:
-        super()._init_()
+    def __init__(self, track) -> None: 
+        super().__init__()  
         self.step = 0
         self.bad_actions = set()
         self.last_action = None
-        self.best_targets = []
-
-
-    def PickMove(self,auto,world, targets, validity):
-        if not targets:
-            return None
-        for move in targets:
-            dx = move[0] - world.car_x
-            dy = move[1] - world.car_y
-            if dx > 0:
-                steering = 1
-            elif dx < 0:
-                steering = -1
-            else:
-                steering = 0
-            if dy > 0:
-                throttle = 1
-            elif dy < 0:
-                throttle = -1
-            else:
-                throttle = 0
-            return (targets)
+        self.last_positions = [] 
 
     def GetName(self):
         return "Luci"
 
-    def step(self, car_state):
+    def PickMove(self, auto, world, targets, validity):
+        if not targets:
+            return None
 
-        self.last_positions.append((car_state.pos.x, car_state.pos.y))  # append adds to the end of the list
-        if len(self.last_positions) > 5: # how many in the list, saves
-            self.last_positions.pop(0) # removes the oldest item
-            
+        # track position history
+        self.last_positions.append((auto.pos.x, auto.pos.y))
+        if len(self.last_positions) > 5:
+            self.last_positions.pop(0)
+
+        # detect if stuck
         stuck = False
         if len(self.last_positions) == 5:
             xs = [p[0] for p in self.last_positions]
-            if max(xs) - min(xs) < 2:  # if the car hasnt moved more than 2 blocks horizontaly, its stuck
-                # it only cares about moving forward, x,
+            if max(xs) - min(xs) < 2:
                 stuck = True
 
-        # if the car crashed, remember the last action as bad
-        if car_state.collided and self.last_action is not None:
-            self.bad_actions.add(self.last_action)
+      
+        valid_moves = [targets[i] for i in range(len(targets)) if validity[i]]
+        pool = valid_moves if valid_moves else list(targets)
+
+        current_x = auto.pos.x
+        current_y = auto.pos.y
+
+       
+        if stuck:
+            return max(pool, key=lambda m: abs(m.x - current_x) + abs(m.y - current_y))
+
         
-        # try random action until we find one not marked as bad
-        while True:
-            steering = random.uniform(-1, 1)
-            throttle = random.uniform(0, 1)
-            action = (round(steering, 1), round(throttle, 1))
-            if action not in self.bad_actions:
-                self.last_action = action  # save action for next frame
-                return steering, throttle
+        for move in pool:
+            if move.x > current_x:
+                return move
+
+        
+        return pool[0]
