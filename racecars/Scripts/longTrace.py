04b03ca6ab557d9753b16b28d7a5da571926883b
Jolanda007 <christik.stumpf@gmail.com>
2026-03-12 14:01:27 +0100

    wallah to nebude fungovat (pridal jsem ray cats length funkci konecne)

--- racecars/Scripts/longTrace.py
+++ racecars/Scripts/longTrace.py
@@ -1,50 +1,51 @@
 import random
 import math
 from simulation.script_api import AutoAuto
-def clamp(n,min,max):
-    if n < min:
-        n = min
-    if n > max:
-        n = max
-    else: return n
+def activeClamp(n,diff,min,max):
+    if n + diff < min:
+        return min
+    if n + diff > max:
+        return max
+    else: return n + diff
 class Auto(AutoAuto):
-    def __init__(self) -> None:
+    def __init__(self,track) -> None:
         super().__init__()
         self.step = 0
-        self.processedWorld = []
+        self.road_mask = track.road_mask
+        self.width = len(track.road_mask)
+        self.height = len(track.road_mask[0])
+        
 
     def GetName(self) -> str:
-        return "longTrace (DO NOT FUCKING RUN I FUKCING BEG)"
+        return "longTrace (DO NOT FUCKING RUN I FUCKING BEG)"
 
     def PickMove(self, auto, world, targets, validity):
         pass
     
-    def preProcess(self,world):
-        width = len(world.road)
-        height = len(world.road[0])
-        for _ in range(width):
-           self.processedWorld.append([])
-           for u in range(height):
-               self.processedWorld[u].append([0,False]) 
-        for x in range(width):
-            #TS WRONG I NEED TO FIX
-            for y in range(height):
-                if world.road[x+1][y+1] == True and world.road[x+1][y-1] == True and world.road[x-1][y+1] == True and world.road[x-1][y-1] == True:
-                    self.processedWorld[x][y][1] == True
-
-    
-    def targetRayCastLength(self, auto, world, target, validity):
+    def targetRayCastLength(self, auto, target):
         rayCastVector = [target[0] -auto.pos.x,auto.pos.y - target[1]]
-        for _ in range(99):
-            try:
-                if not world.road[auto.pos.x + _ * rayCastVector[0]] [auto.pos.y + _ * rayCastVector[1]]:
-                    fullVector = [auto.pos.x + _ * rayCastVector[0],auto.pos.y + _ * rayCastVector[1]]
-                    break
-            except:
-                break
-        pivot = [fullVector[0]*0.5,fullVector[1]*0.5]
-        for _ in range(99):
-            pass
-        
+        for i in range(max(self.width,self.height)):
+                if self.road_mask[auto.pos.x + i * rayCastVector[0]][auto.pos.y + i * rayCastVector[1]] == False:
+                    fullVector = [auto.pos.x + i * rayCastVector[0],auto.pos.y + i * rayCastVector[1]]
+                    carVector = [i * rayCastVector[0], i * rayCastVector[1]]
+                    carVectorConst = carVector[1]/carVector[0]
+                    xi = activeClamp(fullVector[0], 0 , 0 , self.width)
+                    yi = activeClamp(fullVector[1], 0 , 0 , self.width)
+                    if xi != auto.pos.x + i * rayCastVector[0]:
+                        fy = (carVector[1]/carVectorConst) + auto.pos.y
+                        fCoord = [xi, fy]
+                    elif yi != auto.pos.y + i * rayCastVector[0]:
+                        fx = (carVector[0] * carVectorConst) + auto.pos.x
+                        fCoord = [fx, yi]
+                    finVector = [fCoord[0] - auto.pos.x, fCoord[1] - auto.pos.y]
+                    rayCastLength = math.sqrt(pow(finVector[0],2) + pow(finVector[1],2) )
+                    return rayCastLength
+                    
+                    
+                    
+                
+                
+            
+
         
         
