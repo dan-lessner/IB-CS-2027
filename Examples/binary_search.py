1e5a0acbce2c09359691aa27baf2d80091db3202
Pan učitel <lessner@opengate.cz>
2026-03-19 17:33:40 +0100

    binary search example (stoopid teacher edition)

---
+++ Examples/binary_search.py
@@ -0,0 +1,30 @@
+"""
+Measuring the performance of binary search.
+
+It tries to find each element in the given sorted list and counts the number 
+of comparisons needed to find it. Finally, it calculates the average 
+across all box sizes.
+
+Is it correct? How do you make sure?
+"""
+
+items = [145, 687, 58, 68, 278, 149, 296, 382, 398, 426, 827, 654, 257, 12, 16, 8, 147, 1028, 283, 647, 2, 48, 12]
+items = sorted(items)
+
+total = 0
+for pokus in items :
+    count = 0
+    indexhigh = (len(items) - 1)
+    indexlow = (0)  
+    while indexhigh != indexlow: 
+        count += 1
+        index = (indexhigh + indexlow) / (2)
+        hledam = items[int(index)]
+        if hledam == pokus:
+            break
+        elif hledam > pokus:
+            indexhigh = index
+        else:
+            indexlow = index
+    total += count
+print(len(items), total/len(items))
