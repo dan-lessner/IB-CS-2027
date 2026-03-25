e6e0038ff326d6bdf10ad07027695cf7b679ff6f
Ernest <ernest@levkovit.ch>
2026-03-25 09:23:04 +0100

    Im retarded

--- small_exercises/search/ernest.py
+++ small_exercises/search/ernest.py
@@ -4,17 +4,16 @@ def binary_search(arr, target):
             return 0
         else:
             return -1
-    max = len(arr)
     split = len(arr)//2
     idx = 0
     if arr[split] == target:
         return split
     elif target <= arr[split]:
-        idx = binary_search(arr[0:split], target)
+        idx = binary_search(arr[:split], target)
         if idx == -1:
             return -1
     else:
-        idx = binary_search(arr[split:max], target)
+        idx = binary_search(arr[split:], target)
         if idx == -1:
             return -1
         idx = split + idx
