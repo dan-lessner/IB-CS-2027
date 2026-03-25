5b6033a30e512d6361481a4b3359f64970785975
Jolanda007 <christik.stumpf@gmail.com>
2026-03-25 09:56:20 +0100

    test case onto my thingy

--- small_exercises/search/binarySearchChris.py
+++ small_exercises/search/binarySearchChris.py
@@ -1,54 +1,57 @@
 #skibidi pingus
 import math
+import random
 
 #nefunguje, asi budu potrebovat rewrite
 #hardcoded for now cuz im too lazy to do it rn :3, also ik ze ten offset index vec se da udelat lepe ale nebudu to menit protoze bych to stealoval od ernesta
 
 def listSplit(list, element):
-    print("--------------------------------")
-    print("element: " + str(element))
-    print("maxIndex: " + str(len(list)-1))
+    #print("--------------------------------")
+    #print("element: " + str(element))
+    #print("maxIndex: " + str(len(list)-1))
     pivotIndex = math.floor((len(list)-1)/2)
-    print("pivotIndex: " + str(pivotIndex))
-    print("List!: " + str(list))
+    #print("pivotIndex: " + str(pivotIndex))
+    #print("List!: " + str(list))
     
     if list == []:
-        print("TS EMPTY!")
+        #print("TS EMPTY!")
         return None
-    if len(list) == 2:
+    if len(list) == 1:
         if element == list[0]:
             return 0
-        elif element == list[1]:
-            return 1
         else:
             return -1
     
     indx = 0
     
     if element > list[pivotIndex]:
-        print("bigger: " + str(list[pivotIndex: ]))
-        indx =listSplit(list[pivotIndex: ], element)
+        #print("bigger: " + str(list[pivotIndex + 1: ]))
+        indx =listSplit(list[pivotIndex + 1 : ], element)
         if indx == -1:
             return -1
-        indx += pivotIndex
+        indx += pivotIndex + 1
         
     elif element < list[pivotIndex]:
-        print("smaller: " + str(list[: pivotIndex ]))
+        #print("smaller: " + str(list[: pivotIndex ]))
         indx = listSplit(list[: pivotIndex ], element)
         if indx == -1:
             return -1
         
     elif element == list[pivotIndex]:
-        print("Escape!: " + str(pivotIndex))
+        #print("Escape!: " + str(pivotIndex))
         indx = pivotIndex
     else:
         print("Can a loc come up in yo crib? Nah man, fuck you I see you at work!... aww fella dont hate me cuz im beautiful fella. Maybe if you got rid of that yee-yee ass haircut, you'd get some bitches on your dick. Better yet, maybe tanisha dog ass would stop fuckin' with that lawyer or surgeon she fuckin' wit' .... Fella!....... WHAT?")
-    print("indx: " + str(indx))
+    #print("indx: " + str(indx))
     return indx 
 
-list = [1,2,3,4,5,6,7,8,9,10,11]
+list = []
+for x in range(100):
+    
+    list.append(random.randint(0,500))
 
+list.sort()
 
-
-print(listSplit(list, int(input(" on foenem grave what number you wanna check?: "))))
-        
+#print(listSplit(list, int(input(" on foenem grave what number you wanna check?: "))))
+for i in list:
+    print(listSplit(list, i))
