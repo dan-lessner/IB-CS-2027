d89fc9916a1a3508fbc1e58cdd842e9e013a1d26
tomzerzanek <tom@zerzanek.com>
2026-04-22 09:34:54 +0200

    selection sort

--- small_exercises/search/Tom.py
+++ small_exercises/search/Tom.py
@@ -1,1 +1,17 @@
+def selection_sort(arr):
+    n = len(arr)
+    for i in range(n - 1):
+        min_idx = i
+        for j in range(i+1, n):
+            if arr[j] < arr[min_idx]:
+                min_idx = j
+        if min_idx != i:
+            arr[i], arr[min_idx] = \
+            arr[min_idx], arr[i]
+    return arr
+
+
+print(selection_sort(
+  [64, 25, 12, 22, 11]))
+# → [11, 12, 22, 25, 64]
 
